44acbd19645f7e30a0116d354153576f265bd807
jadt7058-ship-it <jadt7058@gmail.com>
2026-08-17 18:56:49 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='mis-compras-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192-1.png','./icon-512-1.png'];
 
 self.addEventListener('install',e=>
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))
